f3249294bb0347aa363e4efa5874c24c093477a6
Sam.wex98 <samwex@udel.edu>
2020-05-21 15:05:03 -0400

    Updates

---
+++ alpha/precache-manifest.983001fff3a3747ef175cae9435726f6.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "048be3e4ca7208ff65f7fd81314515fe",
+    "url": "/educational-game-project-sam-wekselblatt/alphaassets/backgrounds/NextRound.png"
+  },
+  {
+    "revision": "8063552eebb30d656a6bde52a3caffd7",
+    "url": "/educational-game-project-sam-wekselblatt/alphaassets/backgrounds/gameEnd.png"
+  },
+  {
+    "revision": "ac491c0aca151378b4a70f68752357f7",
+    "url": "/educational-game-project-sam-wekselblatt/alphaassets/backgrounds/gameOver.png"
+  },
+  {
+    "revision": "da13aecc4221b4e3d6a07e79b2e60660",
+    "url": "/educational-game-project-sam-wekselblatt/alphaassets/backgrounds/happy_faces.png"
+  },
+  {
+    "revision": "658988ecfd76bfaf47c209eb8d2540e5",
+    "url": "/educational-game-project-sam-wekselblatt/alphaassets/backgrounds/neutral_faces.png"
+  },
+  {
+    "revision": "f94c53916b354fe7c2aeabd13e00ebb3",
+    "url": "/educational-game-project-sam-wekselblatt/alphaassets/backgrounds/upset_faces.png"
+  },
+  {
+    "revision": "e7087d5d91321d575b038366ef9f2407",
+    "url": "/educational-game-project-sam-wekselblatt/alphaassets/citations.md"
+  },
+  {
+    "revision": "e91b59dfdb85b7773f828e0e4977daf8",
+    "url": "/educational-game-project-sam-wekselblatt/alphaassets/hatDisplay.png"
+  },
+  {
+    "revision": "712bdadf77c43fbc4c2a1923bab4ff8a",
+    "url": "/educational-game-project-sam-wekselblatt/alphaassets/hats/tophat_blue.png"
+  },
+  {
+    "revision": "7f26c84b1bf00a6cb8a6d4bce032bd72",
+    "url": "/educational-game-project-sam-wekselblatt/alphaassets/hats/tophat_green_.png"
+  },
+  {
+    "revision": "155ab9ee11915037f7591638d57b0aac",
+    "url": "/educational-game-project-sam-wekselblatt/alphaassets/hats/tophat_orange2.png"
+  },
+  {
+    "revision": "1befa0d16179aa14c432f31288865051",
+    "url": "/educational-game-project-sam-wekselblatt/alphaassets/hats/tophat_pink.png"
+  },
+  {
+    "revision": "2c19969f642b04d2ee93e87c4950a294",
+    "url": "/educational-game-project-sam-wekselblatt/alphaassets/hats/tophat_purple.png"
+  },
+  {
+    "revision": "20f6f3c0ae8e9f058cb0e435ea2f5a89",
+    "url": "/educational-game-project-sam-wekselblatt/alphaassets/hats/tophat_red.png"
+  },
+  {
+    "revision": "4c4802992a0062136d895d824604dd91",
+    "url": "/educational-game-project-sam-wekselblatt/alphaassets/hats/tophat_white.png"
+  },
+  {
+    "revision": "5264ae2d338bec076a5cea05d3967d40",
+    "url": "/educational-game-project-sam-wekselblatt/alphaassets/hats/tophat_yellow.png"
+  },
+  {
+    "revision": "e40b10c937f7618c38c2865b674e1770",
+    "url": "/educational-game-project-sam-wekselblatt/alphaassets/hats/tophat_yellowgreen.png"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "/educational-game-project-sam-wekselblatt/alphaassets/icons/favicon.ico"
+  },
+  {
+    "revision": "8ab30def0ad109a97117bd62db942d75",
+    "url": "/educational-game-project-sam-wekselblatt/alphaassets/icons/icons-16.png"
+  },
+  {
+    "revision": "feee71db2bb3ba5d11b26658baf97783",
+    "url": "/educational-game-project-sam-wekselblatt/alphaassets/icons/icons-192.png"
+  },
+  {
+    "revision": "cb71713be71d0b8820c7ea6af9c5ddc6",
+    "url": "/educational-game-project-sam-wekselblatt/alphaassets/icons/icons-512.png"
+  },
+  {
+    "revision": "200ce541edfaa71be93a1041736d1fee",
+    "url": "/educational-game-project-sam-wekselblatt/alphaassets/icons/icons8-game-controller-16.png"
+  },
+  {
+    "revision": "acc4a4c044ae477b643cd40e8d3e9e7c",
+    "url": "/educational-game-project-sam-wekselblatt/alphaassets/inBetween.png"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "/educational-game-project-sam-wekselblatt/alphafavicon.ico"
+  },
+  {
+    "revision": "79bff2ac6ddf222fe3b3",
+    "url": "/educational-game-project-sam-wekselblatt/alphagen.main.243979dde9558134d9b0.bundle.js"
+  },
+  {
+    "revision": "5e69403aa42e950fedff",
+    "url": "/educational-game-project-sam-wekselblatt/alphagen.vendors.0805bb80903235d40eaf.bundle.js"
+  },
+  {
+    "revision": "0860672783e8c94be8b2ff5e416f6622",
+    "url": "/educational-game-project-sam-wekselblatt/alphaindex.html"
+  },
+  {
+    "revision": "4a040f42f87166b19014de3d57544669",
+    "url": "/educational-game-project-sam-wekselblatt/alphamanifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/educational-game-project-sam-wekselblatt/alphasw.js"
+  }
+]);
